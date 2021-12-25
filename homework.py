d05d3f76afe9cf40da5d142f335520be2f683287
Иван Голенко <igolivanenko1996@gmail.com>
2021-12-25 22:48:35 +0300

    проект 1 с коррект pep8 попытка 3

--- homework.py
+++ homework.py
@@ -74,8 +74,8 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         spent_calories: float = (
-            (self.coeff_calorie_1 * self.get_mean_speed() - 
-             self.coeff_calorie_2) * self.weight 
+            (self.coeff_calorie_1 * self.get_mean_speed()
+             - self.coeff_calorie_2) * self.weight
             / self.M_IN_KM * self.duration * self.min_in_hour)
         return spent_calories
 
@@ -99,8 +99,8 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         spent_calories: float = (
-            (self.coeff_calorie_1 * self.weight + (self.get_mean_speed()**2 
-             // self.weight) * self.coeff_calorie_2 
+            (self.coeff_calorie_1 * self.weight + (self.get_mean_speed()**2
+             // self.weight) * self.coeff_calorie_2
              * self.weight)
             * self.duration * self.min_in_hour)
         return spent_calories
